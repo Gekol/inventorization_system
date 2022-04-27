4485411ebb31289567856bfb39bd9344060b7d6d
Heorhii <heorge.sokolovsky2001@gmail.com>
2022-04-27 19:28:46 +0200

    work_queue implemented

---
+++ rabbitmq_practice/work_queues/new_task.py
@@ -0,0 +1,21 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+for i in range(1, 10):
+    # message = ' '.join(sys.argv[1:]) or b"Hello World!"
+    message = f"{i} message{'.' * i * 4 if i % 2 == 0 else '.' * i * 2}"
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+        ))
+    print(" [x] Sent %r" % message)
+connection.close()
