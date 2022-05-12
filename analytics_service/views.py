b183aa114e4c516a3fac5795af32a48cca5e2eb7
Heorhii <heorge.sokolovsky2001@gmail.com>
2022-05-12 08:27:13 +0200

    intermediate commit

--- analytics_service/views.py
+++ analytics_service/views.py
@@ -1,7 +1,9 @@
 from django.db.models import Count, Q
+from django.http import HttpResponseRedirect
 from rest_framework import permissions, viewsets
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from analytics_service.serializers import ItemTypeSerializer
 from core import IsAdmin
@@ -12,6 +14,22 @@ from inventorization_service.models import ItemType, Item
 class ItemTypesViewSet(viewsets.ModelViewSet):
     queryset = ItemType.objects.all()
     serializer_class = ItemTypeSerializer
+
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(self.queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(self.queryset, many=True)
+        item_types = serializer.data
+        for item_type in item_types:
+            item_type["link"] = f"{self}"
+        print(str(self.request.path))
+        for key in self.request.META:
+            print(key, self.request.META[key])
+        return super().list(request, *args, **kwargs)
+        # return HttpResponseRedirect(redirect_to=f"{self.request.build_absolute_url()}")
 
     def get_permissions(self):
         permission_classes = [permissions.IsAuthenticated]
@@ -31,5 +49,7 @@ class AnalyticsViewSet(viewsets.ViewSet):
         return Response(data)
 
     def get_permissions(self):
-        permission_classes = [permissions.IsAuthenticated, IsAdmin]
+        permission_classes = [permissions.IsAuthenticated]
+        if self.action in ["create", "destroy", "update", "partial_update"]:
+            permission_classes += [IsAdmin]
         return [permission() for permission in permission_classes]
